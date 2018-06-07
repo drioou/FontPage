1861d361ea5013ff8d26a3bb083b1b56b0c11f91
duanyong <1406422800@qq.com>
2018-06-07 14:34:47 +0800

    udpate

--- 06-01.js
+++ 06-01.js
@@ -72,3 +72,56 @@ function Queue() {
 }
 
 // 优先队列
+function PriorityQueue() {
+    let items = [];
+
+    function QueueElement(element, priority) {
+        this.element = element;
+        this.priority = priority;
+    }
+
+    this.enqueue = function (element, priority) {
+        let queueElement = new QueueElement(element, priority);
+        if (this.isEmpty()) {
+            items.push(queueElement);
+        }
+        else {
+            var added = false;
+            for (var i = 0; i < items.length; i++) {
+                if (QueueElement.priority < items[i].priority) {
+                    items.slice(i, 0, queueElement);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added) {
+                items.push(queueElement);
+            }
+        }
+    }
+    this.dequeue = function () {
+        return items.shift();
+
+    }
+
+}
+
+function hotPotato(namelist, num) {
+    var queue = new Queue();
+    for (var i = 0; i < namelist.length; i++) {
+        queue.enqueue(namelist[i]);
+    }
+    var eliminated = '';
+    while (queue.size() > 1) {
+        for (var i = 0; i < num; i++) {
+            queue.enqueue(queue.dequeue());
+        }
+        eliminated = queue.dequeue();
+        console.log(eliminated + "被淘汰了");
+    }
+    return queue.dequeue();
+}
+
+var names = ['a', 'b', 'c', 'd', 'e'];
+var winner = hotPotato(names, 7);
+console.log(winner + ' is winner');
